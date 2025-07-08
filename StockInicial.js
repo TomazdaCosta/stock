23917f8a5e0ab3e2d8ef7b398351a32a8651e708
Tomaz <tomaz@MacBook-Pro-de-Tomaz.local>
2025-07-08 18:43:42 -0300

    Ajuste no stock inicial

--- StockInicial.js
+++ StockInicial.js
@@ -6,39 +6,9 @@ const stockInicial = [
     "productMarketing": "cereais"
   },
   {
-    "id": 2,
-    "productName": "Arroz Vô Olimpio Parboilizado 1kg",
-    "productStock": 3000,
-    "productMarketing": "cereais"
-  },
-  {
-    "id": 3,
-    "productName": "Arroz do Sítio Branco 1kg",
-    "productStock": 1300,
-    "productMarketing": "cereais"
-  },
-  {
-    "id": 4,
-    "productName": "Arroz do Sítio Parboilizado 1kg",
-    "productStock": 1500,
-    "productMarketing": "cereais"
-  },
-  {
     "id": 5,
     "productName": "Feijão de Corda NB 1kg",
     "productStock": 800,
-    "productMarketing": "cereais"
-  },
-  {
-    "id": 6,
-    "productName": "Feijão Carioca NB 1kg",
-    "productStock": 600,
-    "productMarketing": "cereais"
-  },
-  {
-    "id": 7,
-    "productName": "Feijão Branco NB 1kg",
-    "productStock": 300,
     "productMarketing": "cereais"
   },
   {
@@ -66,47 +36,11 @@ const stockInicial = [
     "productMarketing": "hotifruti"
   },
   {
-    "id": 12,
-    "productName": "Melancia",
-    "productStock": 50,
-    "productMarketing": "hotifruti"
-  },
-  {
-    "id": 13,
-    "productName": "Laranja",
-    "productStock": 3000,
-    "productMarketing": "hotifruti"
-  },
-  {
-    "id": 14,
-    "productName": "Maracujá",
-    "productStock": 1000,
-    "productMarketing": "hotifruti"
-  },
-  {
     "id": 15,
     "productName": "Linguiça Calabresa Perdigão 400g",
     "productStock": 200,
     "productMarketing": "frios"
   },
-  {
-    "id": 16,
-    "productName": "Queijo Coalho Peça",
-    "productStock": 200,
-    "productMarketing": "frios"
-  },
-  {
-    "id": 17,
-    "productName": "Queijo Mussarela Peça",
-    "productStock": 300,
-    "productMarketing": "frios"
-  },
-  {
-    "id": 18,
-    "productName": "Presunto Peça",
-    "productStock": 300,
-    "productMarketing": "frios"
-  }
 ]
 
 export default stockInicial
