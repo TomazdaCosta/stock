1cac30f80e88f2bc07accac492444ea025ad486e
Tomaz <tomaz@MacBook-Pro-de-Tomaz.local>
2025-07-08 18:54:59 -0300

    Recuperando conexão com o repositório

--- store/configureStore.jsx
+++
@@ -1,7 +0,0 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import stock from './stock'
-
-const reducer = combineReducers({ stock })
-const store = configureStore({ reducer })
-
-export default store
